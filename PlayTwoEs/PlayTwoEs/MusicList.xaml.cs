48e2a2b8c07cce5b88376771f6c2b375191d66fa
train06 <train06>
2016-08-24 11:42:15 -0700

    First Commit

---
+++ PlayTwoEs/PlayTwoEs/MusicList.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PlayTwoEs
+{
+    public partial class MusicList : ContentPage
+    {
+        public MusicList()
+        {
+            InitializeComponent();
+        }
+    }
+}
